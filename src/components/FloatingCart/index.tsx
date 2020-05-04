fcb1106cfa76b04548c35cda3488e7a197ab70e2
Matheus Moreira <matheustjp111@gmail.com>
2020-05-04 15:32:10 -0300

    Entrega de desafio

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,23 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const quantity = products.reduce((accumulator, product) => {
+      accumulator += product.price * product.quantity;
 
-    return formatValue(0);
+      return accumulator;
+    }, 0);
+
+    return formatValue(quantity);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const quantity = products.reduce((accumulator, product) => {
+      accumulator += product.quantity;
 
-    return 0;
+      return accumulator;
+    }, 0);
+
+    return quantity;
   }, [products]);
 
   return (
